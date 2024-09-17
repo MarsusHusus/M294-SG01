5d83c1f724f49c53f5ffb75bcf44e11ea310658c
MarsusHusus <marvin.huber@edu.gbssg.ch>
2024-09-17 10:58:59 +0200

    Kommentare

--- Tag3/app.js
+++ Tag3/app.js
@@ -68,7 +68,8 @@ function deleteData(id){
 function test1(){
     
 }
-function renderData(){
+
+function renderData() {
     // Daten holen (1)
     $.ajax({
         type: "get",
@@ -76,27 +77,67 @@ function renderData(){
         dataType: "json",
         success: function (response) {
             console.log(response);
-            // JOSON ist da!
+            // JSON ist da!
             // Daten in Tabelle ausgeben und mit handlebars anzeigen
            
-            $.get("auto.hbs", function(template){ // (2)
-                //(3)
-                var compiled = Handlebars.compile(template);
-                var html = compiled(response);
+            $.get("auto.hbs", function(template) { // (2)
+                console.log(template)
+                // (3)
+                let compiled = Handlebars.compile(template);
+                console.log(compiled)
+                let html = compiled(response);
+                console.log(html)
                 // (4)
                 $('main').html(html);
-                // delete - Action (5)
-                $(".delete").on("click", function(){
+                
+                // Delete - Action (5)
+                $(".delete").on("click", function() {
                     console.log("delete");
-                    //let id = $(this).data("id");
                     let id = $(this).attr("data-id");
                     console.log(id);
                     deleteData(id);
                     renderData();
                 });
-              });
+
+                // Tanken - Action (NEW)
+                $(".tanken").on("click", function() {
+                    let id = $(this).attr("data-id");
+
+                    // daten holen von dem einen Datensatz mit id ?
+                    $.ajax({
+                        // holt dateien
+                        type: "GET",
+                        // nur die datensätze mit wo id this ist (Z.104)
+                        url: "./api/api.php?id="+id,
+                        // Wird im datentyp json geladen 
+                        dataType: "json",
+                        success: function (response) {
+                            console.log(response.data[0].tanken);
+                            let tanken = parseInt(response.data[0].tanken) +1;
+                            console.log(tanken);
+                            
+                            
+                            let newData = response.data[0];
+                            newData.tanken = tanken;
+                            console.log(newData);
+                            
+                            
+                            $.ajax({
+                                type: "post",
+                                url: "./api/api.php?id="+id,
+                                data: newData,
+                                dataType: "json",
+                                success: function (response) {
+                                    renderData();
+                                }
+                            });
+                        }
+                    });
+                });
+            });
         }
     });
 }
+
  
 export {renderData, getData, getDataID, insertData, updateData, deleteData};
